f3075703a376faee6d8a3db23c276e4c3377723b
sakthi priyan <sakthipriyans@gmail.com>
2012-12-14 23:38:48 +0530

    Initial commit

---
+++ powerbot/core/Processor.py
@@ -0,0 +1,17 @@
+'''
+Created on 14-Dec-2012
+
+@author: sakthipriyan
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
