2de82708554accaf02221be97cbb8f3779ad4aac
Sakthi Priyan H <sakthipriyans@gmail.com>
2012-12-15 01:41:53 +0530

    SQL query updated

--- powerbot/database/sql.py
+++ powerbot/database/sql.py
@@ -1,13 +1,13 @@
 
 ##Core table where status change is recorded##
 create_state_change = '''CREATE TABLE "state_change" (
-    "timestamp" INTEGER NOT NULL,
+    "timestamp" INTEGER PRIMARY KEY NOT NULL,
     "new_state" INTEGER NOT NULL
 );'''
 
 #Primary daily reports table
 create_daily_reports = '''CREATE TABLE daily_reports (
-    "date" INTEGER NOT NULL,
+    "date" INTEGER PRIMARY KEY NOT NULL,
     "on" INTEGER NOT NULL
 );'''
 
@@ -23,15 +23,14 @@ create_reports = '''CREATE TABLE "reports" (
 create_messages = '''CREATE TABLE "messages" (
     "new_state" INTEGER NOT NULL,
     "message" TEXT NOT NULL,
-    "frequency" INTEGER NOT NULL DEFAULT (0)
+    "usage" INTEGER NOT NULL DEFAULT (0)
 )'''
 
 #used in case of Internet connectivity problem 
 create_tweet_queue = '''CREATE TABLE "tweet_queue" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    "timestamp" INTEGER PRIMARY KEY NOT NULL,
     "type" INTEGER NOT NULL DEFAULT (1),
     "message" TEXT NOT NULL,
     "picture" TEXT NOT NULL,
-    "timestamp" INTEGER NOT NULL,
     "expires" INTEGER NOT NULL
 );'''
