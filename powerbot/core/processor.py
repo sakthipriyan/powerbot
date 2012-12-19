95d91fd781b79dc0fbe4f52f9b0ff7693740b467
Sakthi Priyan H <sakthipriyans@gmail.com>
2012-12-20 02:37:46 +0530

    Processor made to use queue and made multi threaded. DB updated

--- powerbot/core/processor.py
+++ powerbot/core/processor.py
@@ -10,38 +10,59 @@ import threading
 from powerbot.database.models import StateChange, Tweet
 from powerbot.database import access
 import datetime
-from powerbot.core.tweet import post_tweet
+from Queue import Queue
 
 old_status = True
+state_change_queue = Queue()
 
-def do_sensing():
-    while(True):
-        new_status = get_status()
-        global old_status
-        if(new_status != old_status):
-            print str(time.asctime()) + ' Status changed from ' + str(old_status) + ' to '  + str(new_status)
-            old_status = new_status
-            process_change(StateChange(1 if new_status else 0, int(time.time())))
-        time.sleep(1)
-
-def process_change(stateChange):
-    access.new_state_change(stateChange)
+def get_message_with_ts(stateChange):
     message = access.get_message(stateChange)
     flag =  'ON' if stateChange.new_state else 'OFF'
     sign =  datetime.datetime.fromtimestamp(stateChange.timestamp).strftime("  ~ %H:%M "+flag)
-    tweet = Tweet(stateChange.timestamp, message.message + sign, None, stateChange.timestamp + 600)
-    access.new_tweet(tweet)
-    post_tweet(tweet.message)
+    return message.message + sign
 
-def background():
-    print "Generates reports"
+def do_sensing():
+    while True:
+        new_status = get_status()
+        global old_status, state_change_queue
+        if(new_status != old_status):
+            print str(time.asctime()) + ' Status changed from ' + str(old_status) + ' to '  + str(new_status)
+            old_status = new_status
+            state_change = StateChange(1 if new_status else 0, int(time.time()))
+            state_change_queue.put(state_change)
+            print 'Change put into queue ' + str(state_change_queue.qsize())        
+        time.sleep(1)
 
-def main():
-    print 'Starting service'
-    senseThread = threading.Thread(target = do_sensing)
-    processThread = threading.Thread(target = background)
+def process_change():
+    global state_change_queue
+    while True:
+        stateChange = state_change_queue.get()
+        access.new_state_change(stateChange)
+        tweet = Tweet(stateChange.timestamp, get_message_with_ts(stateChange) , None, stateChange.timestamp + 600)
+        access.new_tweet(tweet)
+
+def process_tweets():
+    while True:
+        print 'Trying to send tweet'
+        time.sleep(2)
+
+def process_reports():
+    while True:
+        print "Generates reports"
+        time.sleep(5)
+
+def main():    
+    print 'Starting POWER BOT service'
+    senseThread = threading.Thread(target = do_sensing)    
+    processThread = threading.Thread(target = process_change)
+    tweetThread = threading.Thread(target = process_tweets)
+    reportThread = threading.Thread(target = process_reports)
+    
     senseThread.start()
     processThread.start()
+    tweetThread.start()
+    reportThread.start()
     
 if __name__ == "__main__":
     main()
+
