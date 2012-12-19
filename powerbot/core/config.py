96e9ed3cb58ed768d96412595311d4f54ce2aed2
Sakthi Priyan H <sakthipriyans@gmail.com>
2012-12-19 22:32:04 +0530

    Added twitter config file

---
+++ powerbot/core/config.py
@@ -0,0 +1,11 @@
+'''
+Created on 19-Dec-2012
+
+@author: sakthipriyan
+'''
+
+#TODO - update with your twitter tokens
+twitter_token = 'Insert your twitter token here'
+twitter_secret = 'Insert your twitter secret here'
+oauth_token = 'Insert your oauth token here'
+oauth_token_secret = 'Insert your oauth token secret here'
