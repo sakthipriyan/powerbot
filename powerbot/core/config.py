3aea72cc459f95cbfbf9a18f165c3860c31aed75
Sakthi Priyan H <sakthipriyans@gmail.com>
2013-01-11 01:28:52 +0530

    Updated additional configurations

--- powerbot/core/config.py
+++ powerbot/core/config.py
@@ -9,3 +9,17 @@ twitter_token = 'Insert your twitter token here'
 twitter_secret = 'Insert your twitter secret here'
 oauth_token = 'Insert your oauth token here'
 oauth_token_secret = 'Insert your oauth token secret here'
+
+#Log files
+log_file = '/var/log/powerbot/service.log'
+out_file = '/var/log/powerbot/sysout.log'
+err_file = '/var/log/powerbot/syserr.log'
+
+#SQlite db file
+database_file = '/var/opt/powerbot/database.db'
+
+#Powerbot will send tweet if this file is available
+tweet_file = '/var/opt/powerbot/tweet'
+
+#Process id for self termination
+pid = '/var/run/powerbot.pid'
